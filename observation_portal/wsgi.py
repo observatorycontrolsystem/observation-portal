f41c670e8b4cec9665b5875787010fada77d427f
Jon <jnation@lco.global>
2019-03-22 14:25:19 -0700

    added config file for gunicorn and moved the configuration into there

--- observation_portal/wsgi.py
+++ observation_portal/wsgi.py
@@ -9,28 +9,8 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 
 import os
 
-from apscheduler.schedulers.blocking import BlockingScheduler
-from apscheduler.triggers.cron import CronTrigger
-from observation_portal.requestgroups.tasks import expire_requests
-from observation_portal.accounts.tasks import expire_access_tokens
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'observation_portal.settings')
 
 application = get_wsgi_application()
-
-
-def on_starting(server):
-    scheduler = BlockingScheduler()
-    scheduler.add_job(
-        expire_requests.send,
-        CronTrigger.from_crontab('*/5 * * * *')
-    )
-    scheduler.add_job(
-        expire_access_tokens.send,
-        CronTrigger.from_crontab('0 15 * * *')
-    )
-    try:
-        scheduler.start()
-    except KeyboardInterrupt:
-        scheduler.shutdown()
