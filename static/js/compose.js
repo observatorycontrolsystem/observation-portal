a55d6990ec0ade3d90b05be0cd0ca329eb9a5638
Jon Nation <jnation@lco.global>
2019-04-29 17:58:24 -0700

    switch btn primary to btn info beacause its the same color minus the hover of red. Also modified styling a bit

--- static/js/compose.js
+++ static/js/compose.js
@@ -4,6 +4,7 @@ import $ from 'jquery';
 import BootstrapVue from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
+import '../css/main.css'
 
 import { formatDate } from './utils.js';
 import { csrfSafeMethod, getCookie } from './utils.js';
