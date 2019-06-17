caab04502aea0f35ddcd6299f5e3e5a0fac1aac5
Jon <jnation@lco.global>
2019-06-17 22:47:16 +0000

    re-add tooltip import

--- static/js/request_row.js
+++ static/js/request_row.js
@@ -1,7 +1,7 @@
 import $ from 'jquery';
 import moment from 'moment';
 import {datetimeFormat} from './utils.js';
-
+import tooltip from 'bootstrap';
 import { getThumbnail, getLatestFrame, downloadAll } from './archive.js';
 
 $('.downloadall').click(function(){
