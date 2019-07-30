2cfa0b848252b7fb6a4774904ef1ce50639190d2
Austin Riba <austin@m51.io>
2019-07-30 13:31:24 -0700

    Better method for determining present allocations

--- observation_portal/proposals/tasks.py
+++ observation_portal/proposals/tasks.py
@@ -13,6 +13,6 @@ def time_allocation_reminder():
         # Only send an email if we are within 3 months of the end of the semester
         # and the proposal has at least one allocation.
         if (proposal.current_semester.end - timezone.now()).days <= 93 and \
-                proposal.timeallocation_set.count() > 0:
+                len(proposal.current_allocation) > 0:
             logger.info('Sending time allocation reminder for {}'.format(proposal))
             proposal.send_time_allocation_reminder()
