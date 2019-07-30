4918b4e5419b242575fb92510891b8b6da50b551
Austin Riba <austin@m51.io>
2019-07-30 13:28:59 -0700

    Only send emails if semester end is within 3 months.

--- observation_portal/proposals/tasks.py
+++ observation_portal/proposals/tasks.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 import dramatiq
 import logging
 
@@ -9,8 +10,9 @@ logger = logging.getLogger(__name__)
 @dramatiq.actor()
 def time_allocation_reminder():
     for proposal in Proposal.current_proposals().filter(active=True):
-        if proposal.pi:
+        # Only send an email if we are within 3 months of the end of the semester
+        # and the proposal has at least one allocation.
+        if (proposal.current_semester.end - timezone.now()).days <= 93 and \
+                proposal.timeallocation_set.count() > 0:
             logger.info('Sending time allocation reminder for {}'.format(proposal))
             proposal.send_time_allocation_reminder()
-        else:
-            logger.warn('Proposal {} does not have a PI!'.format(proposal))
