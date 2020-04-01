9f8e0a58b4e8e2fc9aa45aa63dfad1bbe7d68547
eheinrich <eheinrichjosties@gmail.com>
2020-04-01 14:13:04 -0700

    Check first if the proposal has an allocation in the current semester before checking if we are within 3 months of the semester end

--- observation_portal/proposals/tasks.py
+++ observation_portal/proposals/tasks.py
@@ -12,7 +12,9 @@ def time_allocation_reminder():
     for proposal in Proposal.current_proposals().filter(active=True).distinct():
         # Only send an email if we are within 3 months of the end of the semester
         # and the proposal has at least one allocation.
-        if (proposal.current_semester.end - timezone.now()).days <= 93 and \
-                len(proposal.current_allocation) > 0:
+        if (
+                len(proposal.current_allocation) > 0
+                and (proposal.current_semester.end - timezone.now()).days <= 93
+        ):
             logger.info('Sending time allocation reminder for {}'.format(proposal))
             proposal.send_time_allocation_reminder()
