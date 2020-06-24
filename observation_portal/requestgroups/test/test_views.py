2733785b5026ec6f3a3df3da3e2d51a4f7179e67
eheinrich <eheinrichjosties@gmail.com>
2020-06-24 22:49:14 +0000

    Add location and schedulable query params to instrument info endpoint

--- observation_portal/requestgroups/test/test_views.py
+++ observation_portal/requestgroups/test/test_views.py
@@ -1,11 +1,9 @@
 from django.test import TestCase
-from django.contrib.auth.models import User
 from django.urls import reverse
 from django.utils import timezone
 from mixer.backend.django import mixer
 from unittest.mock import patch
 
-from observation_portal.accounts.models import Profile
 from observation_portal.accounts.test_utils import blend_user
 from observation_portal.proposals.models import Proposal, Membership
 from observation_portal.requestgroups.models import RequestGroup, Request, Configuration
@@ -230,6 +228,38 @@ class TestTelescopeStates(TelescopeStatesFromFile):
 
 
 class TestInstrumentInformation(TestCase):
+    def setUp(self):
+        super().setUp()
+        self.staff_user = blend_user(user_params={'is_staff': True})
+
     def test_instrument_information(self):
         response = self.client.get(reverse('api:instruments_information'))
         self.assertIn('1M0-SCICAM-SBIG', response.json())
+
+    def test_instrument_information_for_specific_telescope(self):
+        response = self.client.get(reverse('api:instruments_information') + '?telescope=2m0a')
+        self.assertIn('2M0-FLOYDS-SCICAM', response.json())
+        self.assertNotIn('1M0-SCICAM-SBIG', response.json())
+
+    def test_instrument_information_for_nonexistent_location(self):
+        response = self.client.get(reverse('api:instruments_information') + '?site=idontexist')
+        self.assertEqual(len(response.json()), 0)
+
+    def test_instrument_information_for_specific_instrument_type(self):
+        response = self.client.get(reverse('api:instruments_information') + '?instrument_type=1M0-SCICAM-SBIG')
+        self.assertEqual(len(response.json()), 1)
+        self.assertIn('1M0-SCICAM-SBIG', response.json())
+
+    def test_non_staff_user_can_only_see_schedulable(self):
+        response = self.client.get(reverse('api:instruments_information') + '?only_schedulable=false')
+        self.assertNotIn('1M0-SCICAM-SBXX', response.json())
+
+    def test_staff_user_can_see_non_schedulable_by_default(self):
+        self.client.force_login(self.staff_user)
+        response = self.client.get(reverse('api:instruments_information'))
+        self.assertIn('1M0-SCICAM-SBXX', response.json())
+
+    def test_staff_user_can_request_only_schedulable(self):
+        self.client.force_login(self.staff_user)
+        response = self.client.get(reverse('api:instruments_information') + '?only_schedulable=true')
+        self.assertNotIn('1M0-SCICAM-SBXX', response.json())
