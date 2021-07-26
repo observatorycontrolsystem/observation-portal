2cb14eab32261ba6b6c28b21dc007266bef9edac
Jon <jnation@lco.global>
2021-07-26 20:57:10 +0000

    make grid and line expansion and orientation consistent

--- observation_portal/requestgroups/dither.py
+++ observation_portal/requestgroups/dither.py
@@ -54,7 +54,7 @@ def calc_line_offsets(num_points, point_spacing, orient, center):
     for i in range(0, max(num_points, 0)):
         distance = i*point_spacing + distance_offset
         # Angles measured clockwise from North ("y-axis") rather than anti-clockwise
-        # from East ("x-axis") so sin/cos flipped compared to normal
+        # from East ("x-axis")
         x_offset = (distance * -sino)
         y_offset = (distance * coso)
         offsets.append((x_offset, y_offset))
@@ -84,32 +84,32 @@ def calc_spiral_offsets(num_points, point_spacing):
 def calc_grid_offsets(num_rows, num_columns, point_spacing, line_spacing, orient=90, center=False):
     """Calculates offsets for a grid of <num_points> (must be a square number
     if scalar or a tuple of (n_columns x n_rows) grid points) with <point_spacing>
-    arcseconds apart horizontally and <line_spacing> arcseconds apart vertically,
-    orientated with columns increasing along <orient> degrees East of North
+    arcseconds apart vertically and <line_spacing> arcseconds apart horizontally at 0 orientation,
+    orientated with rows increasing along <orient> degrees East of North
     (clockwise from North through East)
     """
     offsets = []
 
     # A centered grid pattern has the middle of the grid corresponding with an offset of 0
-    row_distance_offset = -(line_spacing * (num_rows-1)) / 2.0 if center else 0.0
-    col_distance_offset = -(point_spacing * (num_columns-1)) / 2.0 if center else 0.0
+    row_distance_offset = -(point_spacing * (num_rows-1)) / 2.0 if center else 0.0
+    col_distance_offset = -(line_spacing * (num_columns-1)) / 2.0 if center else 0.0
     sino = sin(radians(orient))
     coso = cos(radians(orient))
-    rotated_x_offset = coso * col_distance_offset + sino * row_distance_offset
-    rotated_y_offset = -sino * col_distance_offset + coso * row_distance_offset
+    rotated_x_offset = coso * col_distance_offset - sino * row_distance_offset
+    rotated_y_offset = sino * col_distance_offset + coso * row_distance_offset
 
-    for row in range(0, num_rows):
-        if (row % 2) == 0:
-            columns = range(0, num_columns)
+    for column in range(0, num_columns):
+        if (column % 2) == 0:
+            rows = range(0, num_rows)
         else:
-            columns = reversed(range(0, num_columns))
-        for column in columns:
+            rows = reversed(range(0, num_rows))
+        for row in rows:
             # Angles measured clockwise from North ("y-axis") rather than anti-clockwise
-            # from East ("x-axis") so sin/cos flipped compared to normal
-            base_x = column * point_spacing
-            base_y = row * line_spacing
-            x_offset = base_x * coso + base_y * sino + rotated_x_offset
-            y_offset = base_x * -sino + base_y * coso + rotated_y_offset
+            # from East ("x-axis")
+            base_x = column * line_spacing
+            base_y = row * point_spacing
+            x_offset = base_x * coso + base_y * -sino + rotated_x_offset
+            y_offset = base_x * sino + base_y * coso + rotated_y_offset
             offsets.append((x_offset, y_offset))
 
     return offsets
