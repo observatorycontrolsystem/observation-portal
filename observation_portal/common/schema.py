8516652b9116c1a73f554cb34ae1a14c05c52ba6
Matt Daily <mdaily@lco.global>
2021-08-26 18:05:28 -0700

    Add ability for DRF to introspect separate request and response schemas.
    This code is based off of a PR that has been merged to master in the DRF repo, but has not yet been included in a release. See https://github.com/encode/django-rest-framework/pull/7424

--- observation_portal/common/schema.py
+++ observation_portal/common/schema.py
@@ -1,4 +1,6 @@
+from rest_framework import serializers
 from rest_framework.schemas.openapi import AutoSchema
+from rest_framework.schemas.utils import is_list_view
 
 class ObservationPortalSchema(AutoSchema):
     def __init__(self, tags, operation_id_base=None, component_name=None, empty_request=False, is_list_view=True):
@@ -13,11 +15,97 @@ class ObservationPortalSchema(AutoSchema):
 
         return operations
 
-    def get_responses(self, path, method):
-        responses = super().get_responses(path, method)
-        if not self.is_list_view:
-            list_items = responses['200']['content']['application/json']['schema'].pop('items')
-            del responses['200']['content']['application/json']['schema']['type']
-            responses['200']['content']['application/json']['schema'] = list_items
+    def get_components(self, path, method):
+        request_serializer = self.get_request_serializer(path, method)
+        response_serializer = self.get_response_serializer(path, method)
 
-        return responses
+        components = {}
+
+        if isinstance(request_serializer, serializers.Serializer):
+            component_name = self.get_component_name(request_serializer)
+            content = self.map_serializer(request_serializer)
+            components.setdefault(component_name, content)
+
+        if isinstance(response_serializer, serializers.Serializer):
+            component_name = self.get_component_name(response_serializer)
+            content = self.map_serializer(response_serializer)
+            components.setdefault(component_name, content)
+
+        return components
+
+    def get_request_serializer(self, path, method):
+        view = self.view
+
+        if not hasattr(view, 'get_response_serializer'):
+            return view.get_serializer()
+        else:
+            return view.get_response_serializer()
+
+    def get_response_serializer(self, path, method):
+        view = self.view
+
+        if not hasattr(view, 'get_request_serializer'):
+            return view.get_serializer()
+        else:
+            return view.get_request_serializer()
+
+    def get_request_body(self, path, method):
+        if method not in ('PUT', 'PATCH', 'POST'):
+            return {}
+
+        self.request_media_types = self.map_parsers(path, method)
+
+        serializer = self.get_response_serializer(path, method)
+
+        if not isinstance(serializer, serializers.Serializer):
+            item_schema = {}
+        else:
+            item_schema = self._get_reference(serializer)
+
+        return {
+            'content': {
+                ct: {'schema': item_schema}
+                for ct in self.request_media_types
+            }
+        }
+
+    def get_responses(self, path, method):
+        if method == 'DELETE':
+            return {
+                '204': {
+                    'description': ''
+                }
+            }
+
+        self.response_media_types = self.map_renderers(path, method)
+
+        serializer = self.get_request_serializer(path, method)
+
+        if not isinstance(serializer, serializers.Serializer):
+            item_schema = {}
+        else:
+            item_schema = self._get_reference(serializer)
+
+        if is_list_view(path, method, self.view) and self.is_list_view:
+            response_schema = {
+                'type': 'array',
+                'items': item_schema,
+            }
+            paginator = self.get_paginator()
+            if paginator:
+                response_schema = paginator.get_paginated_response_schema(response_schema)
+        else:
+            response_schema = item_schema
+        status_code = '201' if method == 'POST' else '200'
+        return {
+            status_code: {
+                'content': {
+                    ct: {'schema': response_schema}
+                    for ct in self.response_media_types
+                }, 
+                # description is a mandatory property,
+                # https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#responseObject
+                # TODO: put something meaningful into it
+                'description': ""
+            }
+        }
