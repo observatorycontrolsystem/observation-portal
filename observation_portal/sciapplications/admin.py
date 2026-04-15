0e135815b23fe96edaab1c281589bffff24044d6
Jashandeep Sohi <95394050+jashan-lco@users.noreply.github.com>
2026-04-15 13:29:02 -0700

    add admin action to export key data as csv (#354)
    * add admin action to export key data as csv
    
    * output a breakdown of requested time by semester

--- observation_portal/sciapplications/admin.py
+++ observation_portal/sciapplications/admin.py
@@ -7,6 +7,7 @@ from django.utils.safestring import mark_safe
 from django.urls import reverse
 from django.conf import settings
 from django.forms.models import ModelForm
+from django.shortcuts import render
 
 from .models import (
     Instrument, Call, ScienceApplication, TimeRequest, CoInvestigator,
@@ -87,7 +88,7 @@ class ScienceApplicationAdmin(admin.ModelAdmin):
         'preview_link',
     )
     list_filter = (ScienceApplicationTagListFilter, 'call', 'status', 'call__proposal_type')
-    actions = ['accept', 'reject', 'port']
+    actions = ['accept', 'reject', 'port', 'export_key_data_csv']
     search_fields = ['title', 'abstract', 'submitter__first_name', 'submitter__last_name', 'submitter__username']
 
     def preview_link(self, obj):
@@ -145,6 +146,75 @@ class ScienceApplicationAdmin(admin.ModelAdmin):
                         level='ERROR'
                     )
                     return
+
+    @admin.action(description="Export key data as CSV")
+    def export_key_data_csv(self, request, queryset):
+        column_getters = [
+          # name, lambda o: value
+          ("Proposal ID", lambda o: o.id),
+          ("Rank", lambda o: o.tac_rank),
+          ("Title", lambda o: o.title),
+          ("PI Name", lambda o: " ".join([o.pi_first_name, o.pi_last_name])),
+          ("PI Institution", lambda o: o.pi_institution),
+          ("PI Email", lambda o: o.pi),
+          ("Tags", lambda o: "|".join(o.tags))
+        ]
+
+        timerequest_semesters_start = {}
+        for o in queryset:
+            for tr in o.timerequest_set.all():
+                timerequest_semesters_start[tr.semester.id] = tr.semester.start
+
+        timerequest_semesters = [y[0] for y in sorted(timerequest_semesters_start.items(), key=lambda x: x[1])]
+
+        def timerequests_by_inst_type(o, inst_type, semester):
+            for tr in o.timerequest_set.filter(semester__id=semester):
+                if [inst.code for inst in tr.instrument_types.all()] != [inst_type]:
+                    continue
+                yield tr
+
+        def get_queue_time(o, inst_type, semester):
+            ret = 0
+            for tr in timerequests_by_inst_type(o, inst_type, semester):
+                ret += tr.std_time
+            return ret
+
+        def get_rr_time(o, inst_type, semester):
+            ret = 0
+            for tr in timerequests_by_inst_type(o, inst_type, semester):
+                ret += tr.rr_time
+            return ret
+
+        def get_tc_time(o, inst_type, semester):
+            ret = 0
+            for tr in timerequests_by_inst_type(o, inst_type, semester):
+                ret += tr.tc_time
+            return ret
+
+        for semester in timerequest_semesters:
+            for inst_type in settings.SCI_APPS_ADMIN_EXPORT_CSV_INSTRUMENT_TYPES:
+                column_getters.extend([
+                  (f"{semester} {inst_type} Queue", lambda o, inst_type=inst_type, semester=semester: get_queue_time(o, inst_type, semester)),
+                  (f"{semester} {inst_type} RR", lambda o, inst_type=inst_type, semester=semester: get_rr_time(o, inst_type, semester)),
+                  (f"{semester} {inst_type} TC", lambda o, inst_type=inst_type, semester=semester: get_tc_time(o, inst_type, semester)),
+                ])
+
+        rows = []
+        for o in queryset:
+            cols = []
+            for cg in column_getters:
+                cols.append(str(cg[1](o)))
+            rows.append(",".join(cols))
+
+        headers = ",".join([cg[0] for cg in column_getters])
+        csv = "\n".join([headers, "\n".join(rows)])
+        return render(
+            request,
+            "admin/export_key_data_csv.html",
+            context={
+              "csv": csv,
+            }
+        )
 
 admin.site.register(ScienceApplication, ScienceApplicationAdmin)
 
